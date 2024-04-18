8b157dfb6f50ebf65a11c341c3c09990c479e804
ahmettgg <148767652+ahmettgg@users.noreply.github.com>
2024-04-18 16:30:01 +0300

    Update user-data.sh

--- 01-a-Terraform-Docker-Python-Bookstore/user-data.sh
+++ 01-a-Terraform-Docker-Python-Bookstore/user-data.sh
@@ -11,5 +11,5 @@ usermod -a -G docker ec2-user
 curl -SL https://github.com/docker/compose/releases/download/v2.26.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 cd /home/ec2-user && git clone https://github.com/ahmettgg/Devops_My_Projects.git
-cd Devops_My_Projects/terraform-docker-python-bookstore
+cd Devops_My_Projects/01-a-Terraform-Docker-Python-Bookstore
 docker-compose up
